2a775c94ca570bb49cff3d45af3d8f946e7bf91c
GrayUnit <grayunit@outlook.fr>
2020-09-21 14:56:14 +0200

    Ajout des modules, et installations des dépendances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ClientsModule } from './clients/clients.module';
+import { CoreModule } from './core/core.module';
+import { loadTranslations } from '@angular/localize';
+import { TemplatesModule } from './templates/templates.module';
+import { SharedModule } from './shared/shared.module';
+import { LoginModule } from './login/login.module';
+import { UiModule } from './ui/ui.module';
+import { PageNotFoundModule } from './page-not-found/page-not-found.module';
+import { IconsModule } from './icons/icons.module';
+import { TextModule } from './text/text.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +22,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule,
+    ClientsModule,
+    CoreModule,
+    TemplatesModule,
+    SharedModule,
+    LoginModule,
+    UiModule,
+    PageNotFoundModule,
+    IconsModule,
+    TextModule
   ],
   providers: [],
   bootstrap: [AppComponent]
