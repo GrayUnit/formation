ba024513b80b5daeeb891208ce1a9a861eb05a8a
GrayUnit <grayunit@outlook.fr>
2020-09-22 14:14:40 +0200

    Ajout du router, création des pages clients, login, page-not-found. Ajout de la transclusion de router-outler dans l'UI

--- src/app/clients/clients.module.ts
+++ src/app/clients/clients.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientsRoutingModule } from './clients-routing.module';
+import { PageListClientsComponent } from './pages/page-list-clients/page-list-clients.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PageListClientsComponent],
   imports: [
     CommonModule,
     ClientsRoutingModule
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,13 +5,15 @@ import { CoreRoutingModule } from './core-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [HeaderComponent, NavComponent, FooterComponent],
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent,
